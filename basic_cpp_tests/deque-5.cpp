e5e44b9a516fb1e175534e4d9d89a33f4c352f78
RikishK <rikish.kamboj@student.uts.edu.au>
2022-10-06 07:15:21 +0000

    deque updates

--- basic_cpp_tests/deque-5.cpp
+++ basic_cpp_tests/deque-5.cpp
@@ -20,8 +20,10 @@ int main(int argc, char **argv)
   deque<const A*> adeque;
   A a;
   adeque.push_back(&a);
-  adeque.clear();
-  const A *aptr = adeque.front();
+  deque<const A*> bdeque;
+  bdeque = adeque;
+
+  const A *aptr = bdeque.back();
 
   aptr->f(ptr);
 
